2583eccf4b339fafcb064a0c1fed6ce53332ecd4
AllT2709 <renatotorrescalle@gmail.com>
2024-09-02 22:57:10 -0500

    change sizing noice

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -99,7 +99,7 @@ return {
         popupmenu = {
           relative = "editor",
           position = {
-            row = "67%",
+            row = "70%",
             col = "50%",
           },
           size = {
